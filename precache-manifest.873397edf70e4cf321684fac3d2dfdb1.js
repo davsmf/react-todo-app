651a9632213b4f195c449090c495d904e649d9bd
David Meshulam <davsmf@gmail.com>
2020-09-15 18:27:35 +0300

    Updates

---
+++ precache-manifest.873397edf70e4cf321684fac3d2dfdb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748f7b8a02c4dfdd453f56bcde9dcebd",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "3519b9837b620fa2954d",
+    "url": "/react-todo-app/static/css/main.4be66a4b.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/react-todo-app/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3519b9837b620fa2954d",
+    "url": "/react-todo-app/static/js/main.1028d180.chunk.js"
+  },
+  {
+    "revision": "cce3f349551f3963897f",
+    "url": "/react-todo-app/static/js/runtime-main.1ed84029.js"
+  }
+]);
